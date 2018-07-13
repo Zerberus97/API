c7a3159f9db68ac44f4d68159d57b70fde563b03
Zerberus97 <40399674+Zerberus97@users.noreply.github.com>
2018-07-13 09:56:44 -0400

    Todo Ready ;)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,11 +25,11 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
 
-                <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top row" style="background-color:#Cc0000">
+                <nav class="navbar navbar-expand-md  fixed-top row" style="background-color:#Cc0000">
 
                     <div class="container">
                     <!-- <img src="/LogoCompletoBlanco.png" width="160" height="80" class="d-inline-block align-top" alt=""  href="{{ url('/') }}">-->
-                        <a class="navbar-brand" href="{{ url('/') }}">DoorSystem</a>
+                        <a class="navbar-brand" style="color: #ffffff" href="{{ url('/') }}">DoorSystem</a>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation" style="background-color:#ffffff">
                             <span class="navbar-toggler-icon"></span>
                         </button>
@@ -37,13 +37,13 @@
                         <div class="collapse navbar-collapse" id="navbarsExampleDefault" >
                             <ul class="navbar-nav mr-auto">
                                 <li class="nav-item active">
-                                    <a class="nav-link active" href="{{ url('/') }}">Inicio <span class="sr-only">(current)</span></a>
+                                    <a class="nav-link active" style="color: #ffffff" href="{{ url('/') }}">Inicio <span class="sr-only">(current)</span></a>
                                 </li>
                                 <li class="nav-item active">
-                                    <a class="nav-link" href="{{ route('login') }}">Profesores <span class="sr-only">(current)</span></a>
+                                    <a class="nav-link" style="color: #ffffff" href="{{ route('login') }}">Profesores <span class="sr-only">(current)</span></a>
                                 </li>
                                 <li class="nav-item active">
-                                    <a class="nav-link" href="{{ url('/graficosala') }}">Salas <span class="sr-only">(current)</span></a>
+                                    <a class="nav-link" style="color: #ffffff" href="{{ url('/graficosala') }}">Salas <span class="sr-only">(current)</span></a>
                                 </li>
                             </ul>
                         </div>
